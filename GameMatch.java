3c745b483c80c10daa76769d87402c367c6174e3
thu105 <thu105@mail.chapman.edu>
2016-10-16 03:29:15 -0700

    Create GameMatch.java

---
+++ GameMatch.java
@@ -0,0 +1,17 @@
+import java.net.Socket;
+import NimGame;
+
+class GameMatch(Socket, Socket, NimGame) {
+  public final Socket first;
+  public final Socket second;
+  public final NimGame game;
+
+  public GameMatch(final Socket first, final Socket second, NigGame game) {
+    this.first = first;
+    this.second = second;
+    this.game = game;
+  }
+  public boolean hasSocket(Socket soc) {
+    return ((first==soc)||(second==soc));
+  }
+}
