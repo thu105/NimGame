8bc47917fc886699ef10c26d2d45f26925eac3c4
thu105 <thu105@mail.chapman.edu>
2016-10-18 19:33:42 -0700

    Update GameMatch.java

--- GameMatch.java
+++ GameMatch.java
@@ -10,7 +10,7 @@ class GameMatch(Socket, Socket, NimGame) {
     this.second = second;
     this.game = game;
   }
-  public boolean hasSocket(Socket soc) {
+  public boolean hasSocket(Socket soc) {\\check if the given socket is contained in this match
     return ((first==soc)||(second==soc));
   }
   public Socket getOtherSocket(Socket soc){\\get the socket of other player, return null if the given socket is not in the GameMatch
