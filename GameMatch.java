fb0365c4975ac4196d34b2e72162ec4124a28549
thu105 <thu105@mail.chapman.edu>
2016-10-18 19:36:10 -0700

    Update GameMatch.java

--- GameMatch.java
+++ GameMatch.java
@@ -3,9 +3,9 @@ import java.net.Socket;
 class GameMatch(Socket, Socket, NimGame) {
   public final Socket first;
   public final Socket second;
-  public final NimGame game;
+  public final NimBoard game;
 
-  public GameMatch(final Socket first, final Socket second, NigGame game) {
+  public GameMatch(final Socket first, final Socket second, NigBoard game) {
     this.first = first;
     this.second = second;
     this.game = game;
