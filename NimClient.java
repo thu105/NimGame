f6d6eddfcb2708faac5079a0dc4ca17add6c9bd0
thu105 <thu105@mail.chapman.edu>
2016-10-18 21:37:30 -0700

    Update NimClient.java

--- NimClient.java
+++ NimClient.java
@@ -1,1 +1,46 @@
+/**
+* NimClient.java
+*
+* This program will connect to the NimServer so that the user can play a multiplayer Mim game
+* It will not be a multithreaded program because Nim is a turn-based game so no two action needs to simultaneously run
+*/
 
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NimClient{
+  public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Please enter the address of the Nim server (press enter if it is a localhost): ");
+    String userInput= keyboard.nextLine();
+    if(userInput.equals(""))
+      userInput="localhost";
+    int port=7654;
+    System.out.println("Connecting to server on port " + port);
+    
+    try{
+      Socket connectionSock=new Socket(userInput, port);
+      DataOutputStream serverOutput= new DataOutputStrem(connectionSock.getOutputStream());
+      BufferedReader serverInput= new BufferedReader(new InputStreamReader(connectionSock.getInputStream()));
+      System.out.println("Connection made.");
+    }
+    catch (IOException e){
+      System.out.println(e.getMessage());
+      System.exit(0);
+    }
+    System.out.println("Welcome to Nim Game!!");
+    System.out.println("You will be matched with another player to play a Nim game. Here are the rules:");
+    System.out.println("    - There are three heaps of three, four, and five coins respectively.");
+    System.out.println("    - If it is your turn, you can remove ANY amount of coins from a SINGLE heap.");
+    System.out.println("    - The person that removes the last coin wins.");
+    System.out.println("    - The frist player to connect to the server will get the first turn.");
+    System.out.println("--------------------------------------------------------------------------------------");
+    System.out.print("Please enter a name you wished to be addressed as: ");
+    userInput= keyboard.nextLine();
+     
+  }
+}
